f8805066a45a5e5279e243b67d1df2378bd3c42b
anmolduainter <anmolduainter@gmail.com>
2018-03-29 10:41:26 +0530

    Image Manipulations

---
+++ ImageManipulations.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 29 10:38:10 2018
+
+@author: anmol
+"""
+
+import cv2 
+import numpy as np
+import Image
+
+# Function to Convert Image to Numpy array
+
+# Frunction to Convert numpy array into Image
+
+# Function to  Load Image and convert into Gray Scale
+
+# Function to  Draw Partially Transparent Image over another image
