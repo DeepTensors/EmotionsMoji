1782e4671f69da61438a13541a4e8c2790b871eb
anmolduainter <anmolduainter@gmail.com>
2018-03-29 10:50:53 +0530

    Image Manipulations added functions

--- ImageManipulations.py
+++ ImageManipulations.py
@@ -11,9 +11,18 @@ import numpy as np
 import Image
 
 # Function to Convert Image to Numpy array
+def ImageToArray(image):
+    return np.asarray(image)
 
 # Frunction to Convert numpy array into Image
-
+def ArrayToImage(nparray , mode = 'RGB'):
+    return Image.fromarray(np.asarray(np.clip(nparray, 0, 255), dtype='uint8'), mode)
+    
 # Function to  Load Image and convert into Gray Scale
-
+def Load_Image(path):
+    source_image = cv2.imread(source_path)
+    return cv2.cvtColor(source_image, cv2.COLOR_BGR2GRAY)
+    
 # Function to  Draw Partially Transparent Image over another image
+def draw_with_alpha():
+    
